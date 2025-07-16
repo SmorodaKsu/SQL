2b83bbe9e1ad3b7fd088988e42a4211b7ed18bcd
ksu <ksunoonnn@yandex.ru>
2025-07-16 19:32:24 +0300

    Initial commit

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,27 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+
+    public DashboardPage validVerify(String verificationCode) {
+        verify(verificationCode);
+        return new DashboardPage();
+    }
+
+    public void verify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+
+}
---
+++ src/test/java/ru/netology/test/DashboardTest.java
@@ -0,0 +1,26 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class DashboardTest {
+    LoginPage loginPage;
+    DataHelper.AuthInfo authInfo = DataHelper.getAuthInfo();
+
+    @BeforeEach
+    void setUp() {
+        SQLHelper.cleanDatabase();
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    void successfulLogin() {
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
